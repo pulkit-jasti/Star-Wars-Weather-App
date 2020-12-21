c4a9054cae997d2d9e721cf544fc24500e1cbff7
Pulkit Jasti <pulkit.jasti@gmail.com>
2020-12-21 18:53:57 +0530

    Misc. updates

--- script.js
+++ script.js
@@ -30,13 +30,58 @@ function getWeather(lat, long) {
 	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=180aa9da1467549c9761e5a9a2daad88`)
 		.then(response => response.json())
 		.then(result => {
-			console.log(result);
+			//console.log(result);
 			render(result);
 		})
 		.catch(err => {
 			alert(err);
 		});
 }
+
+function render(data) {
+	let temp = document.getElementById('temp');
+	let info = document.getElementById('info');
+	let bckImageContainer = document.getElementById('bck-container');
+	let planetName = document.getElementById('planet-name');
+
+	let temperature = Math.floor(data.main.temp - 273.15);
+	temp.textContent = `${temperature}° C`;
+	info.textContent = `It's a ${data.weather[0].description}, feels like`;
+
+	main = data.weather[0].main;
+	let currentPlanet = null;
+
+	if (temperature > 40) {
+		info.textContent = "It's burning hot, feels like";
+		bckImageContainer.innerHTML = `<img id="bck" src="https://raw.githubusercontent.com/pulkit-jasti/Star-Wars-Weather-App/main/assets/mustafar.jpg" alt="" />`;
+		planetName.textContent = 'Mustafar';
+	} else if (main == 'Clear') {
+		if (temperature > 20) {
+			planetName.textContent = 'Coruscant';
+			let d = new Date().getHours();
+
+			if (d > 19) {
+				bckImageContainer.innerHTML = `<img id="bck" src="https://raw.githubusercontent.com/pulkit-jasti/Star-Wars-Weather-App/main/assets/coruscant-night.jpg" alt="" />`;
+			} else {
+				bckImageContainer.innerHTML = `<img id="bck" src="https://raw.githubusercontent.com/pulkit-jasti/Star-Wars-Weather-App/main/assets/coruscant-day.jpg" alt="" />`;
+			}
+		} else {
+			planetName.textContent = 'Naboo';
+			bckImageContainer.innerHTML = `<img id="bck" src="https://raw.githubusercontent.com/pulkit-jasti/Star-Wars-Weather-App/main/assets/naboo.jpg" alt="" />`;
+		}
+	} else {
+		planetData.forEach(el => {
+			if (el.weather == 'Clear') {
+				currentPlanet = el;
+			}
+		});
+
+		bckImageContainer.innerHTML = `<img id="bck" src="${currentPlanet.imgSrc}" alt="" />`;
+		planetName.textContent = currentPlanet.planetName;
+	}
+}
+
+getLocation();
 
 planetData = [
 	{
@@ -110,58 +155,3 @@ planetData = [
 		imgSrc: 'https://raw.githubusercontent.com/pulkit-jasti/Star-Wars-Weather-App/main/assets/bespin.jpg',
 	},
 ];
-
-function render(data) {
-	let temp = document.getElementById('temp');
-	let info = document.getElementById('info');
-	let bckImageContainer = document.getElementById('bck-container');
-	let planetName = document.getElementById('planet-name');
-
-	temp.textContent = `${Math.floor(data.main.temp - 273.15)}° C`;
-	info.textContent = `It's ${data.weather[0].description}, feels like`;
-
-	main = data.weather[0].main;
-	let currentPlanet = null;
-	//<img id="bck" src="assets/naboo.jpg" alt="" />
-
-	if (Math.floor(data.main.temp - 273.15) > 40) {
-		info.textContent = "It's burning hot, feels like";
-		bckImageContainer.innerHTML = `<img id="bck" src="https://raw.githubusercontent.com/pulkit-jasti/Star-Wars-Weather-App/main/assets/mustafar.jpg" alt="" />`;
-		planetName.textContent = 'Mustafar';
-	} else {
-		console.log(data.weather[0].main);
-		planetData.forEach(el => {
-			if (el.weather == data.weather[0].main) {
-				currentPlanet = el;
-			}
-		});
-
-		bckImageContainer.innerHTML = `<img id="bck" src="${currentPlanet.imgSrc}" alt="" />`;
-		planetName.textContent = currentPlanet.planetName;
-	}
-}
-
-getLocation();
-
-/*switch (data.weather[0].main) {
-			case 'Thunderstorm':
-				bckImage.src = 'random';
-				break;
-			case 'Drizzle':
-				bckImage.src = 'random';
-				break;
-
-			case 'Rain':
-				bckImage.src = 'random';
-				break;
-			case 'Snow':
-				bckImage.src = 'random';
-				break;
-
-			case 'Snow':
-				bckImage.src = 'random';
-				break;
-
-			default:
-				break;
-		}*/
